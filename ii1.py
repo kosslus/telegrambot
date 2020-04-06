53f58cd4bebc654cfd8f5d2482a96e14035dbc11
Tom Riddle <61736005+kosslus@users.noreply.github.com>
2020-04-06 20:08:40 +0300

    v1

---
+++ ii1.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai, json
+updater = Updater(token='')
+dispatcher = updater.dispatcher
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, пообщаемся?')
+def textMessage(bot, update):
+    request = apiai.ApiAI('').text_request()  # Dialogflowок
+    request.lang = 'ru'  
+    request.session_id = 'BatlabAIBot'  
+    request.query = update.message.text 
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech']
+    if response:
+        bot.send_message(chat_id=update.message.chat_id, text=response)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='мая твая не панимать!')
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+updater.start_polling(clean=True)
+updater.idle()
